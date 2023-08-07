71a46a9bfb5bfdaa187790662c8444dae87a308b
ohmyshapez <87324667+ohmyshapez@users.noreply.github.com>
2023-08-07 22:34:31 +0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,16 +1,17 @@
-addLayer("2", {
-    name: "Square", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "2", // This appears on the layer's node. Default is the id with the first letter capitalized
+
+addLayer("3", {
+    name: "Cube", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "3", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
-		lines: new Decimal(0),
+        unlocked: false,
+		faces: new Decimal(0),
     }},
     color: "#4BDC13",
-    requires: new Decimal(4), // Can be a function that takes requirement increases into account
-    resource: "faces", // Name of prestige currency
-    baseResource: "lines", // Name of resource prestige is based on
-    baseAmount() {return player.lines}, // Get the current amount of baseResource
+    requires: new Decimal(6), // Can be a function that takes requirement increases into account
+    resource: "cells", // Name of prestige currency
+    baseResource: "faces", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -20,9 +21,9 @@ addLayer("2", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "2", description: "2d: Reset for lines", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "3", description: "3d: Reset for faces", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
